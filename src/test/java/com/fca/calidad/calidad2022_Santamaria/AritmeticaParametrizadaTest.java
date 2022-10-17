d59fbd508f9289e18ecfd1e4a29b9ddf4b1e17c4
fca <fca@FCA-CC-C-21.inet.uady.mx>
2022-10-17 18:39:30 -0500

    ejecicio completado

--- src/test/java/com/fca/calidad/calidad2022_Santamaria/AritmeticaParametrizadaTest.java
+++ src/test/java/com/fca/calidad/calidad2022_Santamaria/AritmeticaParametrizadaTest.java
@@ -15,33 +15,46 @@ import java.util.Collection;
 
 @RunWith(Parameterized.class)
 public class AritmeticaParametrizadaTest {
-	private double arg1;
-	private double arg2;
-	private double expected;
+	private float arg1;
+	private float arg2;
+	private float arg3;
 	private Calculadora miCalculadora; //Object
+	private algebra mycalculadora;
 	
-	public AritmeticaParametrizadaTest (double arg1, double arg2, double expected) {
+	public AritmeticaParametrizadaTest (float arg1, float arg2, float arg3) {
 		this.arg1 = arg1;
 		this.arg2 = arg2;
-		this.expected = expected;
+		this.arg3 = arg3;
 	}
 
 	//set parameters to test
 	@Parameters
 		public static Collection<Object[]>data() {
 			return Arrays.asList(new Object[][] {
-				{4,2,8},			//Integers
-				{6,-3,-18},			//Negative Integer
-				{-5,-5,25}
+				{4,2,2},			//Integers
+				{6,-3,-2},			//Negative Integer
+				{5,5,1},
+				{5,2,2.5f},
+				{5,-2,-2.5f},
 				});
 		}
 	@Before
 	public void setUp() throws Exception {
-		
+		//Create new object
+		mycalculadora = new algebra();
 	}
 	
 	@After
 	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void Division_Test() {
+		//Using MamCrest
+		//Exercise code to run and test
+		float resEjecucion = mycalculadora.division(this.arg1, this.arg2);
 		
+		//Verify
+		assertThat(this.arg3, is(resEjecucion));
 	}
 }
